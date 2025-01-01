85db1cfc0cbb37f6713a62a0a78c8b4eb84df734
Suraj <soorajwizard01@gmail.com>
2025-01-01 14:16:48 +0300

    added built-in menu and diabled cors incase it interfare with the laravel broadcast

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, Menu } = require("electron");
 
 function createWindow() {
-    const {width, height} = require('electron').screen.getPrimaryDisplay().workAreaSize;
-    const win = new BrowserWindow({
-        width: width,
-        height: height,
-        fullscreen: true,
-        autoHideMenuBar: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: true,
-            enableRemoteModule: true,
-        }
-    });
+  const { width, height } =
+    require("electron").screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({
+    width: width,
+    height: height,
+    fullscreen: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      enableRemoteModule: true,
+      webSecurity: false, // Disable CORS
+      allowRunningInsecureContent: true, // Allow insecure content
+    },
+  });
 
-    win.loadFile('index.html');
+  win.loadFile("index.html");
+
+  const template = [
+    {
+      label: "Menu",
+      submenu: [
+        { label: "Refresh", click: () => win.reload() },
+        {
+          label: "Link Screen",
+          click: () => win.loadURL("https://nitx.io/ns"),
+        },
+        { label: "Quit", click: () => app.quit() },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
 
 app.whenReady().then(() => {
-    app.setLoginItemSettings({openAtLogin: true});
+  app.setLoginItemSettings({ openAtLogin: true });
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  }
 });
